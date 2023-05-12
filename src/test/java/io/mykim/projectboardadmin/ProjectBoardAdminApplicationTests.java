67000ad362624dea808e6505ad4d2b818fc9ac43
mykim8710 <mykim8710@gmail.com>
2023-05-12 17:31:09 +0900

    [MAIN] 게시판 관리자 서비스 프로젝트 생성 - 프로젝트 생성 - 개발에 필요한 의존성 확인

---
+++ src/test/java/io/mykim/projectboardadmin/ProjectBoardAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mykim.projectboardadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBoardAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

780bc79e983e5ec42ee98ed68c8ea77c89f2a47f
mykim8710 <mykim8710@gmail.com>
2023-05-19 18:29:07 +0900

    #13 - root 경로 엔드포인트 설정 및 view 파일 추가

---
+++ src/main/java/io/mykim/projectboardadmin/ViewController.java
@@ -0,0 +1,15 @@
+package io.mykim.projectboardadmin;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class ViewController {
+    @GetMapping("/")
+    public String indexView() {
+        log.info("[GET] /  >>  root index view");
+        return "index";
+    }
+}

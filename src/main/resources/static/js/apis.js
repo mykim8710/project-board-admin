9a1769f58fc3a5007c59601e8f995c6bb005e27e
mykim <mykim8710@gmail.com>
2023-05-25 22:10:29 +0900

    #31 - 게시글관리 frontend 개발완료

---
+++ src/main/resources/static/js/apis.js
@@ -0,0 +1,41 @@
+
+async function callFindAllArticlesApi() {
+    let searchKeyword = document.getElementById('searchInput').value;
+    let page = currentPage;
+    let size = document.getElementById('articles-size').value;
+    let sort = sortCondition.concat(",").concat(sortDirection);
+
+    const res = await fetch(`/api/v1/management/articles?searchKeyword=${searchKeyword}&page=${page}&size=${size}&sort=${sort}`, {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    }).catch(error => {
+        console.log(error);
+    });
+    return await res.json();
+}
+
+async function callFindOneArticleApi(articleId) {
+    const res = await fetch(`/api/v1/management/articles/${articleId}`, {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    }).catch(error => {
+        console.log(error);
+    });
+    return await res.json();
+}
+
+async function callRemoveArticleApi(articleId) {
+    const res = await fetch(`/api/v1/management/articles/${articleId}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    }).catch(error => {
+        console.log(error);
+    });
+    return await res.json();
+}
